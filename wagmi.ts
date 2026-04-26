adbbab82df939ba8990387c00699f4fa8ca13c9b
DenisReaver <145904927+DenisReaver@users.noreply.github.com>
2026-04-26 16:06:05 +0300

    Update wagmi.ts

--- wagmi.ts
+++ wagmi.ts
@@ -1,9 +1,9 @@
 // wagmi.ts
 import { http, createConfig } from 'wagmi';
-import { sepolia } from 'wagmi/chains';
+import { sepolia, baseSepolia } from 'wagmi/chains';
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
 
-// ARC Testnet
+// ARC Testnet (CASTOM NETWORK)
 const arcTestnet = {
   id: 5042002,
   name: 'ARC Testnet',
@@ -14,10 +14,11 @@ const arcTestnet = {
 
 export const config = getDefaultConfig({
   appName: 'Morgen OFT Creator',
-  projectId: '6cd43074381dca42cc0f029143e150c8', // ← Замени на свой!
-  chains: [sepolia, arcTestnet],
+  projectId: '6cd43074381dca42cc0f029143e150c8', // MY ID PROHECT
+  chains: [sepolia, baseSepolia, arcTestnet],     // 
   transports: {
     [sepolia.id]: http('https://ethereum-sepolia-rpc.publicnode.com'),
+    [baseSepolia.id]: http('https://sepolia.base.org'),     // RPC Base Sepolia
     [arcTestnet.id]: http('https://arc-testnet.drpc.org'),
   },
   ssr: true,
